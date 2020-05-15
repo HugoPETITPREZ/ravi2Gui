953b609ff0148656aa044357990aedcfd3518556
Hugop <hugo.petitprez@hotmail.com>
2020-05-15 14:51:45 +0200

    Sauvegarde

--- gui.py
+++ gui.py
@@ -1,19 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QWidget, QVBoxLayout, QTabWidget, QPushButton, \
+    QInputDialog, QLineEdit
 
 class Gui(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
         self.statusBar().showMessage('Ready')
 
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Statusbar')
+        menubar = self.menuBar()
+        fichierMenu = menubar.addMenu("Fichier")
+
+        openAct = QAction("Ouvrir",self)
+        openAct.triggered.connect(self.open)
+        openAct.setShortcut('ctrl+O')
+        openAct.setStatusTip('Ouvrir un fichier')
+
+        recAct = QAction("Enregistrer",self)
+        recAct.triggered.connect(self.rec)
+        recAct.setShortcut('ctrl+S')
+        recAct.setStatusTip('Enregistrer un fichier')
+
+
+        quitAct = QAction("Quitter",self)
+        quitAct.triggered.connect(self.exit)
+        quitAct.setShortcut('ctrl+Q')
+        quitAct.setStatusTip('Quitter')
+
+
+
+        fichierMenu.addAction(openAct)
+        fichierMenu.addAction(recAct)
+        fichierMenu.addSeparator()
+        fichierMenu.addAction(quitAct)
+
+        self.setMinimumSize(1280, 720)
+
+        self.setWindowTitle('Ravi Example')
+
+        self.myWidget = MyTableWidget(self)
+
+        self.setCentralWidget(self.myWidget)
+
         self.show()
 
+    def open(self):
+        print("open")
 
+    def rec(self):
+        print("rec")
+
+    def exit(self):
+        print("exit")
+        self.quit
